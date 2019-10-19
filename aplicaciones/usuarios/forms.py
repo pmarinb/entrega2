86cdef5d46a59941c536881a6d1f86e6c79cd7a8
Pablo Marin Bormuth <p.marinb@alumnos.duoc.cl>
2019-10-19 19:23:07 -0300

    Agregado form y view para login

---
+++ aplicaciones/usuarios/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth import authenticate
+
+
+class Login(forms.Form):
+    nombre_usuario = forms.CharField()
+    contrasenia = forms.CharField(widget=forms.PasswordInput)
+
+    #metodo que limpia los campos
+    def limpiar(self, *args, **kwargs):
+        nombre_usuario = self.cleaned_data.get('nombre_usuario')
+        contrasenia = self.cleaned_data.get('contrasenia')
+
+        #valida que ambos campos esten digitados
+        if usuario and contrasenia:
+
+            #el metodo 'authenticate' valida que ambos campos coincidan y devuelve un
+            #objeto de tipo 'User'
+            usuario= authenticate(username=nombre_usuario,password=contrasenia)
+
+            if not usuario:
+                raise forms.ValidationError('Usuario No Existe')
+            if not usuario.check_password(contrasenia):
+                raise forms.ValidationError('Contraseña Incorrecta')
+        #se llama al metodo y limpia todo
+        return super(Login, self).clean(*args, **kwargs)
+
--- aplicaciones/usuarios/views.py
+++ aplicaciones/usuarios/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import Usuario
+from .forms import Login
 # Create your views here.
+
+
+def vista_login(request):
+    next = request.GET.get('next')
+    form = Login(request.POST or None)
+    if form.is_valid():
+        nombre_usuario = form.cleaned_data.get('nombre_usuario')
+        contrasenia = form.cleaned_data.get('contrasenia')
+        usuario = authenticate(username=nombre_usuario, password=contrasenia)
+        login(request, usuario)
+        if next:
+            return redirect(next)
+        return redirect('/home')
+    return render(request, "login.html", {'form': form})
